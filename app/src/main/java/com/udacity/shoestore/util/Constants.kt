68c586b548a3e28075450025b1feb2ae6cc3c79c
shryash12 <dr.shradhasrathod@gmail.com>
2021-05-05 23:55:02 +0800

    util class created

---
+++ app/src/main/java/com/udacity/shoestore/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.udacity.shoestore.util
+
+object Constants {
+    const val KEY_MY_SHARE_PREF = "etsdgsfdgfsd"
+    const val KEY_MY_SHARE_EMAIL = "KEY_MY_SHARE_EMAIL"
+    const val KEY_MY_SHARE_PASSWORD = "KEY_MY_SHARE_PASSWORD"
+
+}
---
+++ app/src/main/java/com/udacity/shoestore/util/UserManager.kt
@@ -0,0 +1,32 @@
+package com.udacity.shoestore.util
+
+import android.content.Context
+
+
+
+object UserManager {
+
+    fun saveUserLogin(context: Context, email: String, password: String) {
+        val sharePref = context.getSharedPreferences(Constants.KEY_MY_SHARE_PREF, Context.MODE_PRIVATE)
+        val editor = sharePref.edit()
+        editor.putString(Constants.KEY_MY_SHARE_EMAIL, email)
+        editor.putString(Constants.KEY_MY_SHARE_PASSWORD, password)
+        editor.apply()
+    }
+
+    fun getUserLogin(context: Context): Pair<String, String> {
+        val sharePref = context.getSharedPreferences(Constants.KEY_MY_SHARE_PREF, Context.MODE_PRIVATE)
+        val email = sharePref.getString(Constants.KEY_MY_SHARE_EMAIL, "") ?: ""
+        val password = sharePref.getString(Constants.KEY_MY_SHARE_PASSWORD, "") ?: ""
+        return Pair(email, password)
+    }
+
+    fun logOut(context: Context) {
+        val sharePref = context.getSharedPreferences(Constants.KEY_MY_SHARE_PREF, Context.MODE_PRIVATE)
+        sharePref
+            .edit()
+            .clear()
+            .apply()
+
+    }
+}
