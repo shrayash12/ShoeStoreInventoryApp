11ef78480565247f4801524a0a921dd8e9a704cd
shryash12 <dr.shradhasrathod@gmail.com>
2021-05-05 23:55:57 +0800

    ShoeDetails.kt reimplemented

--- app/src/main/java/com/udacity/shoestore/views/shoedetails/ShoeDetails.kt
+++ app/src/main/java/com/udacity/shoestore/views/shoedetails/ShoeDetails.kt
@@ -3,22 +3,47 @@ package com.udacity.shoestore.views.shoedetails
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import androidx.navigation.Navigator
 import com.udacity.shoestore.R
+import com.udacity.shoestore.models.Shoe
+import com.udacity.shoestore.views.shoelisting.ShoeListViewModel
 
 class ShoeDetails : Fragment(R.layout.shoedetailsfragment) {
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val etShoeName = view.findViewById<EditText>(R.id.etShoeName)
+        val etCompanyName = view.findViewById<EditText>(R.id.etCompanyName)
+        val etShoeSize = view.findViewById<EditText>(R.id.etShoeSize)
+        val etDescription = view.findViewById<EditText>(R.id.etDescription)
+
+
         val buttonCancel = view.findViewById<Button>(R.id.buttonCancel)
         val buttonSave = view.findViewById<Button>(R.id.buttonSave)
+
         buttonCancel.setOnClickListener {
             Navigation.findNavController(it)
                 .navigate(R.id.action_shoeDetails_to_shoeListingFragment)
         }
         buttonSave.setOnClickListener {
+            val shoeName = etShoeName.text.toString()
+            val companyName = etCompanyName.text.toString()
+            val shoeSize = etShoeSize.text.toString()
+            val shoeDescription = etDescription.text.toString()
+
+            val bundle = bundleOf(
+                "shoeName" to shoeName,
+                "company" to companyName,
+                "shoesSize" to shoeSize,
+                "shoeDescription to" to shoeDescription
+            )
             Navigation.findNavController(it)
-                .navigate(R.id.action_shoeDetails_to_shoeListingFragment)
+                .navigate(R.id.action_shoeDetails_to_shoeListingFragment, bundle)
 
         }
 
