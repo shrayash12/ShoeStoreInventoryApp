a8eac807e4305ceeeed97047b5908c0967e6fb07
shryash12 <dr.shradhasrathod@gmail.com>
2021-05-05 23:58:31 +0800

    implemented with menuinflator

--- app/src/main/java/com/udacity/shoestore/views/shoelisting/ShoeListingFragment.kt
+++ app/src/main/java/com/udacity/shoestore/views/shoelisting/ShoeListingFragment.kt
@@ -1,41 +1,65 @@
 package com.udacity.shoestore.views.shoelisting
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.ScrollView
-import android.widget.TextView
+import android.view.*
+import android.widget.*
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.udacity.shoestore.R
+import com.udacity.shoestore.models.Shoe
+import com.udacity.shoestore.util.UserManager
 import android.widget.LinearLayout as LinearLayout1
 
 class ShoeListingFragment : Fragment(R.layout.shoelistingfragment) {
+
     lateinit var shoeListViewModel: ShoeListViewModel
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
         val buttonAdd = view.findViewById<FloatingActionButton>(R.id.buttonAdd)
 
         val linearLayout = view.findViewById<LinearLayout1>(R.id.linearLayout)
         val scrollView = view.findViewById<ScrollView>(R.id.scrollViewParent)
+
+
+        val shoe = arguments?.let {
+            val name = it.getString("shoeName")
+            val companyName = it.getString("company")
+            val shoeSize = it.getDouble("shoesSize")
+            val description = it.getString("shoeDescription")
+            Shoe(name ?: "", shoeSize, companyName ?: "", description ?: "")
+        }
+
+        if (shoe != null) {
+            shoeListViewModel.addNewShoes(shoe)
+        }
+
         buttonAdd.setOnClickListener {
             Log.i("ShoeListingFragment", "button Clicked")
             Navigation.findNavController(it)
                 .navigate(R.id.action_shoeListingFragment_to_shoeDetails)
         }
 
-        shoeListViewModel = ViewModelProvider(this).get(ShoeListViewModel::class.java)
         shoeListViewModel.shoesList.observe(requireActivity(), Observer {
 
             it.forEach {
                 val view =
-                    LayoutInflater.from(requireContext()).inflate(R.layout.list_item, scrollView, false)
+                    LayoutInflater.from(requireContext())
+                        .inflate(R.layout.list_item, scrollView, false)
                 val imageView = view.findViewById<ImageView>(R.id.shoeImage)
                 val shoeName = view.findViewById<TextView>(R.id.tvShoeName)
                 val companyName = view.findViewById<TextView>(R.id.tvCompanyName)
@@ -43,12 +67,27 @@ class ShoeListingFragment : Fragment(R.layout.shoelistingfragment) {
                 companyName.text = it.company
                 linearLayout.addView(view)
             }
-
-
         })
-
-
+    }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+       inflater.inflate(R.menu.shoe_menu, menu)
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_logout -> {
+                UserManager.logOut(requireContext())
+                Navigation.findNavController(requireView())
+                    .navigate(R.id.action_shoeListingFragment_to_logInFragment)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        shoeListViewModel = ViewModelProvider(this).get(ShoeListViewModel::class.java)
+        shoeListViewModel.initiaseData()
+    }
 }
