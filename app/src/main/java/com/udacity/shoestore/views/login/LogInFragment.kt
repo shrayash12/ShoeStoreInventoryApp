582618c53258c3453b0012f420f37a38330893d3
shryash12 <dr.shradhasrathod@gmail.com>
2021-05-05 23:57:20 +0800

    User logIn implemented and accordingly user navigated

--- app/src/main/java/com/udacity/shoestore/views/login/LogInFragment.kt
+++ app/src/main/java/com/udacity/shoestore/views/login/LogInFragment.kt
@@ -1,23 +1,46 @@
 package com.udacity.shoestore.views.login
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.udacity.shoestore.R
+import com.udacity.shoestore.util.UserManager
 
 class LogInFragment : Fragment(R.layout.loginfragment) {
+
+    private lateinit var etEmailId: EditText
+    private lateinit var editTextPassword: EditText
+    private lateinit var buttonLogIn: Button
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        // Check if it is log in
+        if (UserManager.getUserLogin(requireContext()).first.isNotEmpty()) {
+            // Navigate to shoe list screen
+            Navigation.findNavController(view)
+                .navigate(R.id.action_logInFragment_to_shoeListingFragment)
+        }
+        etEmailId = view.findViewById(R.id.etEmailid)
+        editTextPassword = view.findViewById(R.id.editTextPassword)
+        buttonLogIn = view.findViewById(R.id.buttonLogIn)
 
-        Log.d(LogInFragment::class.java.simpleName,"OnView Created")
-
-        val buttonLogIn = view.findViewById<Button>(R.id.buttonLogIn)
         buttonLogIn.setOnClickListener {
-            Log.d(LogInFragment::class.java.simpleName,"Button Click")
+            if (etEmailId.text.toString().isEmpty()) {
+                return@setOnClickListener
+            }
+            if (editTextPassword.text.toString().isEmpty()) {
+                return@setOnClickListener
+            }
+            UserManager.saveUserLogin(
+                requireActivity(),
+                etEmailId.text.toString(),
+                editTextPassword.text.toString()
+            )
 
+            // Navigate to welcome screen
             Navigation.findNavController(it).navigate(R.id.action_logInFragment_to_welcomeFragment)
         }
     }
