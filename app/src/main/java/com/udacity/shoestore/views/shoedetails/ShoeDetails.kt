691e9f800fa5f5326d5dbc468ce79e49d91a589e
shryash12 <dr.shradhasrathod@gmail.com>
2021-05-14 14:18:10 +0800

    onCreateView implemented with dataBinding in ShoeDetails.kt

--- app/src/main/java/com/udacity/shoestore/views/shoedetails/ShoeDetails.kt
+++ app/src/main/java/com/udacity/shoestore/views/shoedetails/ShoeDetails.kt
@@ -1,32 +1,36 @@
 package com.udacity.shoestore.views.shoedetails
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.ShoedetailsfragmentBinding
+import com.udacity.shoestore.databinding.ShoedetailsfragmentBindingImpl
+import com.udacity.shoestore.databinding.ShoelistingfragmentBinding
 import com.udacity.shoestore.util.Constants
+import kotlinx.android.synthetic.main.shoedetailsfragment.*
 
-class ShoeDetails : Fragment(R.layout.shoedetailsfragment) {
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val etShoeName = view.findViewById<EditText>(R.id.etShoeName)
-        val etCompanyName = view.findViewById<EditText>(R.id.etCompanyName)
-        val etShoeSize = view.findViewById<EditText>(R.id.etShoeSize)
-        val etDescription = view.findViewById<EditText>(R.id.etDescription)
-        val buttonCancel = view.findViewById<Button>(R.id.buttonCancel)
-        val buttonSave = view.findViewById<Button>(R.id.buttonSave)
-
-        buttonCancel.setOnClickListener {
-            Navigation.findNavController(it)
+class ShoeDetails : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: ShoedetailsfragmentBinding =
+            DataBindingUtil.inflate(inflater, R.layout.shoedetailsfragment, container, false)
+        binding.buttonCancel.setOnClickListener { view: View ->
+            Navigation.findNavController(view)
                 .navigate(R.id.action_shoeDetails_to_shoeListingFragment)
         }
-        buttonSave.setOnClickListener {
+       binding.buttonSave.setOnClickListener { view: View ->
             val shoeName = etShoeName.text.toString()
             val companyName = etCompanyName.text.toString()
             val shoeSize = etShoeSize.text.toString()
@@ -38,10 +42,10 @@ class ShoeDetails : Fragment(R.layout.shoedetailsfragment) {
                 Constants.KEY_SHOE_SIZE to shoeSize,
                 Constants.KEY_SHOE_DESCRIPTION to shoeDescription
             )
-            Navigation.findNavController(it)
+            Navigation.findNavController(view)
                 .navigate(R.id.action_shoeDetails_to_shoeListingFragment, bundle)
-
         }
-        (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.shoe_details)
+        return binding.root
+
     }
 }
