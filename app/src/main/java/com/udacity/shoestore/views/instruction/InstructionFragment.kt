b95d3327149660a7af34023518579eef72982da5
shryash12 <dr.shradhasrathod@gmail.com>
2021-05-14 14:16:05 +0800

    onCreateView implemented with dataBinding in InstructionFragment.kt.kt

--- app/src/main/java/com/udacity/shoestore/views/instruction/InstructionFragment.kt
+++ app/src/main/java/com/udacity/shoestore/views/instruction/InstructionFragment.kt
@@ -1,20 +1,33 @@
 package com.udacity.shoestore.views.instruction
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.InstructionsfragmentBinding
+import com.udacity.shoestore.databinding.WelcomefragmentBinding
 
-class InstructionFragment : Fragment(R.layout.instructionsfragment) {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val buttonShoeStore = view.findViewById<Button>(R.id.buttonShoeStore)
-        buttonShoeStore.setOnClickListener {
-            Navigation.findNavController(it).navigate(R.id.action_instructionFragment_to_shoeListingFragment)
+class InstructionFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: InstructionsfragmentBinding =
+            DataBindingUtil.inflate(inflater, R.layout.instructionsfragment, container, false)
+        binding.buttonShoeStore.setOnClickListener { view: View ->
+            Navigation.findNavController(view)
+                .navigate(R.id.action_instructionFragment_to_shoeListingFragment)
         }
-        (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.instruction_screen)
+        return binding.root
+        (activity as AppCompatActivity).supportActionBar?.title =
+            getString(R.string.instruction_screen)
+
     }
 }
