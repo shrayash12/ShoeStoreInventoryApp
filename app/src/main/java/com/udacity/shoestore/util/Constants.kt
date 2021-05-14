7a82cd0628ab3132391505ceb9db78092f988454
shryash12 <dr.shradhasrathod@gmail.com>
2021-05-15 00:17:33 +0800

    KEY_IS_ON_BOARDING_DONE added

--- app/src/main/java/com/udacity/shoestore/util/Constants.kt
+++ app/src/main/java/com/udacity/shoestore/util/Constants.kt
@@ -4,6 +4,7 @@ object Constants {
     const val KEY_MY_SHARE_PREF = "etsdgsfdgfsd"
     const val KEY_MY_SHARE_EMAIL = "KEY_MY_SHARE_EMAIL"
     const val KEY_MY_SHARE_PASSWORD = "KEY_MY_SHARE_PASSWORD"
+    const val KEY_IS_ON_BOARDING_DONE = "KEY_IS_ON_BOARDING_DONE"
     const val KEY_SHOE_NAME=  "shoeName"
     const val KEY_COMPANY_NAME=  "company"
     const val KEY_SHOE_SIZE=  "shoesSize"
