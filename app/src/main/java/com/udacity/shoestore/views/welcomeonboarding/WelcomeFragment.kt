47ea35db9fe35c9bcf54c72bac70c77741927577
shryash12 <dr.shradhasrathod@gmail.com>
2021-05-14 14:15:34 +0800

    onCreateView implemented with dataBinding in WelcomeFragment.kt

--- app/src/main/java/com/udacity/shoestore/views/welcomeonboarding/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/views/welcomeonboarding/WelcomeFragment.kt
@@ -1,21 +1,32 @@
 package com.udacity.shoestore.views.welcomeonboarding
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.LoginfragmentBinding
+import com.udacity.shoestore.databinding.WelcomefragmentBinding
+import com.udacity.shoestore.util.UserManager
 
-class WelcomeFragment : Fragment(R.layout.welcomefragment) {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val buttonGo = view.findViewById<Button>(R.id.buttonGo)
-        buttonGo.setOnClickListener {
-            Navigation.findNavController(it)
+class WelcomeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: WelcomefragmentBinding =
+            DataBindingUtil.inflate(inflater, R.layout.welcomefragment, container, false)
+        binding.buttonGo.setOnClickListener { view: View ->
+            Navigation.findNavController(view)
                 .navigate(R.id.action_welcomeFragment_to_instructionFragment)
         }
+        return binding.root
         (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.welcome_screen)
     }
 }
