ca1d7615694937b86c91b99bbc894a10702c29b9
shryash12 <dr.shradhasrathod@gmail.com>
2021-05-18 22:39:16 +0800

    added some shoe in initialiseData method in ShoeListViewModel

--- app/src/main/java/com/udacity/shoestore/views/shoelisting/ShoeListViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/views/shoelisting/ShoeListViewModel.kt
@@ -20,34 +20,44 @@ class ShoeListViewModel : ViewModel() {
             ObservableField("Affordable and Good"),
             emptyList()
         )
-        /* val shoeNewBalance = Shoe(
-             "NB",
-             2.5, "NewBalance",
-             "Nice Color and Comfortable", emptyList()
-         )
-         val shoeNike = Shoe(
-             "Nike",
-             5.0, "Nike",
-             "Best shoes for running", emptyList()
-         )
-         val shoeAdidas = Shoe(
-             "Sneaker",
-             5.0, "Adidas",
-             "Daily wear Sneaker", emptyList()
-         )
-         val shoePuma = Shoe(
-             "Loafer",
-             5.5, "Puma",
-             "Comfortable", emptyList()
-         )
-         val shoeAldo = Shoe(
-             "Loafer",
-             4.5, "Aldo",
-             "Stylish", emptyList()
-         )*/
+        val shoeNewBalance = Shoe(
+            ObservableField("NB"),
+            ObservableField("2.5"),
+            ObservableField("NewBalance"),
+            ObservableField("Nice Color and Comfortable"),
+            emptyList()
+        )
+        val shoeNike = Shoe(
+            ObservableField("NkShoe"),
+            ObservableField("5.0"),
+            ObservableField("Nike"),
+            ObservableField("Best shoes for running"),
+            emptyList()
+        )
+        val shoeAdidas = Shoe(
+            ObservableField("Sneaker"),
+            ObservableField("5.5"),
+            ObservableField("Adidas"),
+            ObservableField("Daily wear Sneaker"),
+            emptyList()
+        )
+        val shoePuma = Shoe(
+            ObservableField("Loafer"),
+            ObservableField("5.5"),
+            ObservableField("Puma"),
+            ObservableField("Comfortable"),
+            emptyList()
+        )
+        val shoeAldo = Shoe(
+            ObservableField("Loafer"),
+            ObservableField("5.5"),
+            ObservableField("Aldo"),
+            ObservableField("Stylish"),
+            emptyList()
+        )
         shoesListLocal.value =
-                //  arrayListOf(shoeBata, shoeNewBalance, shoeNike, shoeAdidas, shoePuma, shoeAldo)
-            arrayListOf(shoeBata)
+            arrayListOf(shoeBata, shoeNewBalance, shoeNike, shoeAdidas, shoePuma, shoeAldo)
+        arrayListOf(shoeBata)
     }
 
     fun addNewShoes(shoe: Shoe) {
